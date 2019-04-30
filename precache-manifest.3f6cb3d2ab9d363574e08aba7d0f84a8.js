3c88e0160135ca380d81e64f3b64766959789d57
atiftariq786 <atiftariq786@hotmail.com>
2019-04-30 00:51:33 -0700

    Updates

---
+++ precache-manifest.3f6cb3d2ab9d363574e08aba7d0f84a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a333173b07914c489e5",
+    "url": "/React-Google-Book-Auth/./static/js/runtime~main.7db87463.js"
+  },
+  {
+    "revision": "a1755cd4a5c8c4922fc8",
+    "url": "/React-Google-Book-Auth/./static/js/main.f91427c7.chunk.js"
+  },
+  {
+    "revision": "71032f7c930380829c74",
+    "url": "/React-Google-Book-Auth/./static/js/2.deea14b7.chunk.js"
+  },
+  {
+    "revision": "a1755cd4a5c8c4922fc8",
+    "url": "/React-Google-Book-Auth/./static/css/main.3d2c4b32.chunk.css"
+  },
+  {
+    "revision": "71032f7c930380829c74",
+    "url": "/React-Google-Book-Auth/./static/css/2.6c836918.chunk.css"
+  },
+  {
+    "revision": "b839a91ca4ad7fb7a8359507ab0db9fd",
+    "url": "/React-Google-Book-Auth/./index.html"
+  }
+];
